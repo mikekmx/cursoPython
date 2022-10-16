5bf3ea0b5e9dc8a80d48cd90a8898380f5351f10
Korela Innovacion <kinnergia@gmail.com>
2022-10-16 12:17:56 -0500

    one-commit

---
+++ redSocial/Red_Network.py
@@ -0,0 +1,12 @@
+print('Hola, Bienvenido a Red de Negocios')
+print(""" 
+RED DE NEGOCIOS DE DON MIKE
+""")
+
+nombre = input('Por favor escribe tu nombre y apellido ')
+print("Hola" + " " + nombre + " " + "Bienvenid@ a esta RED DE NEGOCIOS")
+
+año_nacimiento = int(input('Para preparar tu perfil, por favor ingresa tu año de nacimiento '))
+edad = 2022 - año_nacimiento-1
+print(edad)
+
